f2b30b9019abae02fa45b8d50d4524ac5a0be988
Nontchai Tantiponsawas <nontchai@Nontchais-MacBook-Pro.local>
2017-05-05 17:06:19 +0700

    Add first modal Component

---
+++ src/modal/modal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './modal.css'
+class App extends Component {
+  render() {
+    return (
+      <div className={`bodywindow`}>
+        <p>start component</p>        
+        <button>Show Dialog</button>
+      </div>
+    )
+  }
+}
+
+export default App;
