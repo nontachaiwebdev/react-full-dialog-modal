7644553b006308c11fb2b10da5b0e0d18410d196
Nontchai Tantiponsawas <nontchai@Nontchais-MacBook-Pro.local>
2017-05-05 17:55:57 +0700

    add Feature Close Dialog

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App extends Component {
   constructor(props){
     super(props)
     this.pressButton = this.pressButton.bind(this)
+    this.closeModal = this.closeModal.bind(this)
     this.state = {
       showModal : false
     }
@@ -11,13 +12,19 @@ class App extends Component {
   pressButton(){
     this.setState({ showModal : true })
   }
+  closeModal(){
+    this.setState({ showModal : false })
+  }
   render() {
     const { showModal } = this.state
     return (
       <div>
         <p>start component</p>        
         <button onClick={ this.pressButton } >Show Dialog</button>
-        <Modal showModal={ showModal } />
+        <Modal showModal={ showModal } >
+	  <p>This is Modal</p>
+	  <button onClick={ this.closeModal } >Close Dialog</button>
+	</Modal>
        </div>
     )
   }
--- src/modal/modal.js
+++ src/modal/modal.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import './modal.css'
 class Modal extends Component {
   render() {
-    const { showModal } = this.props
+    const { showModal, children } = this.props
     const showCss = showModal ? { 'top' : '0%' } : { 'top':'100%' }
     return (
       <div className={`modalwindow`} style={showCss} >
-        Dialog modal Show!!!
+        { children }  
       </div>
     )
   }
